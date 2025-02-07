1e8be8377f99ad80007bac27c86cb3f79e72feb3
Stefano Antonelli <stefano.antonelli.2024@gmail.com>
2025-02-07 15:03:18 +0100

    feat: added DI for H2 and JPA and repository folder, changed application.properties

--- simpleWebApp/src/main/java/com/example/simpleWebApp/model/Product.java
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/model/Product.java
@@ -1,13 +1,22 @@
 package com.example.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data // from lombok
 @AllArgsConstructor // create a constructor
+@Entity // entità nel database
 public class Product {
 
+    @Id
     private int prodId;
     private String name;
     private int price;
+
+    //costruttore vuoto chiesto da hybernate
+    public Product() {
+
+    }
 }
--- simpleWebApp/src/main/java/com/example/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/service/ProductService.java
@@ -1,61 +1,54 @@
 package com.example.simpleWebApp.service;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.simpleWebApp.model.Product;
+import com.example.simpleWebApp.repository.ProductRepo;
 
 @Service
 
 public class ProductService {
 //creo array di prodocts 
 
-    private final List<Product> products = new ArrayList<>();
+    @Autowired
+    ProductRepo repo; //inietto dipendenze della repo product
 
-    public ProductService() {
-        // Aggiungo gli elementi iniziali alla lista mutabile
-        products.add(new Product(1, "Iphone", 500));
-        products.add(new Product(2, "Samsung", 600));
-        products.add(new Product(3, "Oppo", 450));
-    }
-
+    // private final List<Product> products = new ArrayList<>();
+    // public ProductService() {
+    //     // Aggiungo gli elementi iniziali alla lista mutabile
+    //     products.add(new Product(1, "Iphone", 500));
+    //     products.add(new Product(2, "Samsung", 600));
+    //     products.add(new Product(3, "Oppo", 450));
+    // }
     // GET
     public List<Product> getProducts() { //prendo lista di dati in model/Products
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prodId) { //prendo singolo elemento in lista di dati in model/Products
-        return products.stream()
-                .filter(p -> p.getProdId() == prodId)
-                .findFirst()
-                .orElse(new Product(0, "No Items", 0));
+        return repo.findById(prodId)
+                .orElse(new Product(prodId, null, 0));
+
     }
 
     //  POST
     public void addProduct(Product prod) {
-        products.add(prod);
+        repo.save(prod);
     }
 
     // PUT
     public void updateProduct(Product prod) {
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == prod.getProdId()) {
-                products.set(i, prod); // Sostituisco l'elemento nella lista
-                break;
-            }
-        }
+        repo.save(prod);
+
     }
 
     // DELETE
     public void deleteProduct(int prodId) {
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == prodId) {
-                products.remove(prodId); //rimuovo prodotto dalla lista
-                break;
-            }
-        }
+        repo.deleteById(prodId);
 
     }
 
