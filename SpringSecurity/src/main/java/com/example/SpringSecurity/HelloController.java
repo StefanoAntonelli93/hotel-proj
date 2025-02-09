3da81251b85a0ac11b6f07c494b601a7f9fb9b97
Stefano Antonelli <stefano.antonelli.2024@gmail.com>
2025-02-09 01:30:56 +0100

    feat: added Student, StudentController and CSRF TOKEN

--- SpringSecurity/src/main/java/com/example/SpringSecurity/HelloController.java
+++ SpringSecurity/src/main/java/com/example/SpringSecurity/HelloController.java
@@ -3,10 +3,12 @@ package com.example.SpringSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String greet() {
-        return "Hello!";
+    public String greet(HttpServletRequest request) {
+        return "Welcome to SpringSecurity " + request.getSession().getId();
     }
 }
